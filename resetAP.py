f12632acab508d1118cff7b77ebefcf01425b369
os055891 <oseias.flores@intelbras.com.br>
2021-03-26 16:29:02 -0300

    Implemented request delete for factory reset discovered DUTs

--- resetAP.py
+++ resetAP.py
@@ -9,11 +9,25 @@ import struct
 import binascii
 import time
 from functools import reduce
+import requests
+import json
 
 MCAST_GRP = '233.89.188.1' 
 MCAST_PORT = 10001
 data_itb = struct.pack("BBBB", 0x49, 0x54, 0x42, 0x53)
 data_dlb = struct.pack("BBBB", 0x00, 0x00, 0x00, 0x00)
+
+login = """
+{
+    "data": {
+    "username":'admin',
+    "password":'lockzeus'
+    }
+}
+"""
+headers = {
+  'Content-Type': 'application/json'
+}
 
 def binaryToDevice(data):
         lst = []
@@ -90,14 +104,60 @@ def binaryToDevice(data):
         return device
 
 def resetapi(a, b):
+  i = 0
+  p = 1
   #Print modelo / IP
   print("\nIniciando processo de reset dos equipamentos!")
   print("Equipamentos que serão resetados ao padrão de fábrica: \n")
-  for elem in b:
-    print(f"Dut Model: {b}")
-    for elem in a:
-      print(f"IP: {a}\n")
+  
   #Inicio tratamento requests
+  for elem in a:
+    urlbase ='http://'+a[i]+':80'
+    #print(urlbase)
+    print(f"{p}) DUT {a[i]}  | {b[i]}")
+
+    response = requests.post(urlbase + '/cgi-bin/api/v3/system/login', data=login, headers=headers)
+    sresponse = str(response)
+
+    # Verificar se logou com sucesso no DUT
+    if sresponse == '<Response [200]>':
+        print("\nLOGIN EFETUADO COM SUCESSO!!!")
+        state = True
+    else:
+        print("Falha no Login: {}".format(response))
+        state = False
+
+    if state == True:
+        # Armazenar json response
+        auth_token = response.json()
+
+        # Filtrando apenas o value token e ajustando o token para uso no novo headers
+        toke = auth_token.get('data')
+        toke = toke.get('Token')
+        tokef = 'Token ' + toke
+
+        services = {
+            'Authorization': tokef
+        }
+
+        print(f"\nRestaurando padrão de fábrica do DUT {p}")
+        response = requests.delete(urlbase + '/cgi-bin/api/v3/system/config', headers=services)
+        
+        #Tratar exceptions aqui...  TO DO
+        print("Sucesso!")
+        
+        #factory = response.json()
+        #print(factory)
+        
+    else:
+        print("Falha ao tentar resetar o DUT...\n")
+        print("Tente novamente..\n")
+        #menu()
+
+    #Incrementador das listas de IP e Modelo    
+    i = i + 1
+    p = p + 1
+    
 # Uma vez
 def main():
   sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
@@ -105,10 +165,11 @@ def main():
   sock.sendto(data_itb, (MCAST_GRP, MCAST_PORT))
   #sock.sendto(data_dlb, (MCAST_GRP, MCAST_PORT))
   # sock.setblocking(0)
+  apip = []
+  apmodel = []
   while True:
+    sock.settimeout(1)
     try:
-      apip = []
-      apmodel = []
       print("Realizando Discovery de APs...\n")
       #Comentado envia o packet fora do laço (linha 99) envia apenas uma vez
       #sock.sendto(data_itb, (MCAST_GRP, MCAST_PORT))
@@ -116,19 +177,23 @@ def main():
       #print ('received %s bytes from %s : %s' % (len(data), address, binaryToDevice(data)))
       takemodel = binaryToDevice(data)
       print (f'AP Encontrado: {address[0]} \nModel: {takemodel["model"]}!')
-      #time.sleep(1)
-      while True:
-        print("\nRealizar um novo discovery? \n")
-        saidoloop = input("Utilize [s] ou [n]:")
-
-        if saidoloop == 's' or saidoloop == 'S':
-          break
-        if saidoloop == 'n' or saidoloop == 'N':
-          return resetapi(apip,apmodel)
-
-        print("Opção Invalida! Utilize [s] ou [n]...")
-
-    except socket.error:
+      apip.append(address[0])
+      apmodel.append(takemodel['model'])
       time.sleep(1)
+   
+    except socket.error or socket.timeout:
+      print("Fim do Discover...\n")
+      return resetapi(apip,apmodel)
 
+      #time.sleep(1)
+
+    #print("\nRealizar um novo discovery? \n")
+    #saidoloop = input("Utilize [s] ou [n]:")
+
+    #if saidoloop == 's' or saidoloop == 'S':
+    #  
+    #if saidoloop == 'n' or saidoloop == 'N':
+    #  return resetapi(apip,apmodel)
+    #
+    #    print("Opção Invalida! Utilize [s] ou [n]...")
 main()
